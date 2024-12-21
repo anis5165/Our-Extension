1519d962898ad93d626f7a3740007851c26c7e48
Anish Kumar <anishkumargvm02@gmail.com>
2024-12-21 10:37:34 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import ObjectDetect from './components/ObjectDetection/ObjectDetect';
+import  HandPose  from './components/HandGestures/HandPose';
+import FacialLandmark from './components/FacialLandmarkDetection/FacialLandmark'
+
+
+const App = () => {
+  return (
+    <div>
+      {/* <ObjectDetect />q */}
+      {/* <HandPose/> */}
+      <FacialLandmark />
+    </div>
+  )
+}
+
+export default App
