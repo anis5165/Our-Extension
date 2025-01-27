969749c9087333ec0c9d13063d868b137e02f91c
Anish Kumar <anishkumargvm02@gmail.com>
2025-01-27 15:03:19 +0530

    add extension elements

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,8 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import ObjectDetect from './components/ObjectDetection/ObjectDetect.jsx';
+import HandPose from './components/HandGestures/HandPose.jsx';
+import FacialLandmark from './components/FacialLandmarkDetection/FacialLandmark.jsx';
 
 const rootEle = document.getElementById('root');
 
@@ -16,10 +18,27 @@ if (rootEle) {
 
 
 const ObjectDetectElement = document.getElementById("object-detection");
+const HandPoseElement = document.getElementById("hand-pose");
+const FacialLandmarkElement = document.getElementById("facial-landmark");
 
 if(ObjectDetectElement){
   createRoot(ObjectDetectElement).render(
       <ObjectDetect />
   )
 
+}
+else if(HandPoseElement){
+  createRoot(HandPoseElement).render(
+      <HandPose />
+  )
+
+}
+else if(FacialLandmarkElement){
+  createRoot(FacialLandmarkElement).render(
+      <FacialLandmark />
+  )
+
+}
+else {
+  <div>No Extension loaded.</div>
 }
